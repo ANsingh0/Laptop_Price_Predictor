979478e24519d55c14ef673f4e3ffa09b2a413e5
ANsingh0 <singhnarayanakshay@gmail.com>
2023-04-09 19:23:56 +0530

    Adding Laptop price prediction app code and jupyter notebook code

---
+++ app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Import the model
+pipe = pickle.load(open("pipe.pkl","rb"))
+df = pickle.load(open("df.pkl","rb"))
+
+st.title("Laptop Predictor")
+
+#Brand
+company = st.selectbox("Brand", df["Company"].unique())
+
+#Type
+type = st.selectbox("Type", df["TypeName"].unique())
+
+#Ram
+ram = st.selectbox("RAM(in GB)",[2,4,6,8,12,16,24,32,64])
+
+#Weight
+weight = st.number_input("Weight of Laptop")
+
+#Touchscreen
+touchscreen = st.selectbox("Touchscreen", ["No","Yes"])
+
+#IPS
+ips = st.selectbox("IPS", ["No", "Yes"])
+
+#Screen Size
+screen_size = st.number_input("Screen Size")
+
+#Resolution
+resolution = st.selectbox("Screen Resolution", ["1920x1000", '1366x768',
+                                                '1600x900', '3840x2160', '3200x1800', '2800x1800',
+                                                '2560x1600','2560x1440', '2304x1440'])
+#CPU
+cpu = st.selectbox("CPU", df["Cpu Brand"].unique())
+
+#Memory
+hdd = st.selectbox("HDD(in GB)", [0,128,256,512,1024,2048])
+
+ssd = st.selectbox("SSD(in GB)", [0,8,128,256,512,1024])
+
+gpu =st.selectbox("GPU",df["Gpu Brand"].unique())
+
+os =st.selectbox("OS",df["OS"].unique())
+
+if st.button("Predict Price"):
+    # Query
+    ppi = None
+    if touchscreen == "Yes":
+        touchscreen = 1
+    else:
+        touchscreen = 0
+
+    if ips== "Yes":
+        ips = 1
+    else:
+        ips = 0
+
+    X_res = int(resolution.split("x")[0])
+    Y_res = int(resolution.split("x")[1])
+    ppi = ((X_res**2)+ (Y_res**2))**0.5/screen_size
+    query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+
+    query = query.reshape(1,12)
+    st.title("The Predicted price of this configuration is "+ str(int(np.exp(pipe.predict(query)[0]))))
